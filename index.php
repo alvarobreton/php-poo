653db7c6a586cf8771fdd6e38883998ebef4b790
Alvaro Breton <alvarolimadb@gmail.com>
2021-10-04 17:44:58 -0500

    Crear clase Persona metodo de mostrar y guardar... metodo intermedio de formato

--- index.php
+++ index.php
@@ -1,18 +1,31 @@
 <?php
 
     class Persona{
-        public $nombre = 'Pedro';
+
+        public $nombre = array();
+        public $apellido = array();
 
 
-        public function hablar($mensaje){
-            echo $mensaje;
+        public function guardar($nombre, $apellido){
+            $this->nombre[] = $nombre;
+            $this->apellido[] = $apellido;
+        }
+
+        public function mostrar(){
+            for ($i=0; $i < count($this->nombre); $i++) { 
+                self::formato($this->nombre[$i], $this->apellido[$i]); //$this o Persona 
+            }
+        }
+
+        public function formato($nombre, $apellido)
+        {
+            echo 'Nombre: '.$nombre.' | Apeelido: '.$apellido.'<br>';
         }
     }
 
+    $persona = new Persona();
+    $persona->guardar('Álvaro','Bretón');
+    $persona->guardar('Carlos','Lima');
 
-    $persona =  new Persona();
-
-    //echo $persona->nombre;
-    $persona->hablar('Este es un saludo alvarobreton.com');
-
+    $persona->mostrar();
 ?>
